ed0b341fdc7432275e935005fc9fbd6579c7fd2c
drblobfish <jules.herrmann@gmail.com>
2021-05-10 22:29:57 +0200

    added second box

--- generate.py
+++ generate.py
@@ -1,7 +1,17 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size = [1,1,1])
+length= 1
+width= 2
+height = 3
+
+x= 0
+y = 0
+z = height/2
+
+pyrosim.Send_Cube(name="Box1", pos=[x,y,z], size = [length,width,height])
+
+pyrosim.Send_Cube(name="Box2", pos=[x+length,y+width,z+height], size = [length,width,height])
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -10,7 +10,7 @@ p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane.urdf")
 
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 while True:
 	time.sleep(1/60)
