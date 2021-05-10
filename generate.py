987a2852f762a444f7ea46e423b6990a2ad4c25e
drblobfish <jules.herrmann@gmail.com>
2021-05-10 23:48:17 +0200

    3 box robot

--- generate.py
+++ generate.py
@@ -22,17 +22,15 @@ def Create_World():
 def Create_Robot():
 	pyrosim.Start_URDF("body.urdf")
 
-	length= 1
-	width= 1
-	height = 1
 
-	x= 0
-	y = 0
-	z = height/2
+	pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5], size = [1,1,1])
 
-	pyrosim.Send_Cube(name="Torso", pos=[x,y,z], size = [length,width,height])
-	pyrosim.Send_Cube(name="Leg", pos=[length/2,0,height/2], size = [length,width,height])
-	pyrosim.Send_Joint(name="Torso_Leg", parent="Torso",child="Leg", type="revolute",position=f"{length/2} 0 {height}")
+	pyrosim.Send_Joint(name="Torso_Leg_F", parent="Torso",child="LegFront", type="revolute",position="0.5 0 1")
+	pyrosim.Send_Cube(name="LegFront", pos=[0.5,0,-.5], size = [1,1,1])
+
+	pyrosim.Send_Joint(name="Torso_Leg_B", parent="Torso",child="LegBack", type="revolute",position="-0.5 0 1")
+	pyrosim.Send_Cube(name="LegBack", pos=[-0.5,0,-.5], size = [1,1,1])
+
 
 	pyrosim.End()
 
