e379a79e506372784c5fefe952029a7dddb17a21
drblobfish <jules.herrmann@gmail.com>
2021-05-10 22:51:56 +0200

    tower

--- generate.py
+++ generate.py
@@ -3,15 +3,20 @@ import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("boxes.sdf")
 
 length= 1
-width= 2
-height = 3
+width= 3
+height = 1
 
 x= 0
 y = 0
 z = height/2
 
-pyrosim.Send_Cube(name="Box1", pos=[x,y,z], size = [length,width,height])
+hauteur_tour = 10
 
-pyrosim.Send_Cube(name="Box2", pos=[x+length,y+width,z+height], size = [length,width,height])
+current_height = 0
+
+for i in range(hauteur_tour):
+	pyrosim.Send_Cube(name="Box"+str(i), pos=[x,y,current_height+height * (0.9**i)/2], size = [length * (0.9**i),width * (0.9**i),height * (0.9**i)])
+	#print(current_height)
+	current_height+=height * (0.9**(i))
 
 pyrosim.End()
