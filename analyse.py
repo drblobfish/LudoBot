ac06fc80eed65dc02c399bc7acdf6f2d168a1e62
drblobfish <jules.herrmann@gmail.com>
2021-05-14 22:32:46 +0200

    prettifying the dataviz

--- analyse.py
+++ analyse.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 backLegData = np.load("data/backLegSensorValues.npy")
 frontLegData = np.load("data/frontLegSensorValues.npy")
 
-plt.plot(backLegData)
-plt.plot(frontLegData)
+plt.plot(backLegData, label="back Leg Sensor",linewidth=3)
+plt.plot(frontLegData, label="front Leg Sensor")
+plt.legend()
 plt.show()
